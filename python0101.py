6bd0ce579ca0e860514fcf15f997ac76305024a6
coderlzy <lzyliangzi@163.com>
2018-09-23 23:39:03 +0800

    python learning 01

---
+++ python0101.py
@@ -0,0 +1,14 @@
+####################
+# 新年贺卡
+# python0101.py
+# 2018
+####################
+name1=input("请输入收卡人：")
+name2=input("请输入送卡人：")
+print("####################")
+print(name1)
+print()
+print("happy new year to you")
+print()
+print("          yours",name2)
+print("####################")
