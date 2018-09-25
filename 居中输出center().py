0baf069b355c68a2711ff33656abc58bce53e047
coderlzy <lzyliangzi@163.com>
2018-09-25 11:38:55 +0800

    morning python practice

---
+++ 居中输出center().py
@@ -0,0 +1,6 @@
+#居中输出
+#print(s.center(width,[fillchar]))
+string1="小许你好"
+string2="原子很坏"
+print(string1.center(75))
+print(string2.center(75,"*"))
---
+++ 打印信封.py
@@ -0,0 +1,13 @@
+#打印信封，人名由用户输入
+name=input("欢迎来此打印信封，请输入收信人名字：")
+line1="300384"
+line2="天津理工大学"
+line4="人民邮电出版社 100164"
+print(line1.center(50))
+print(line2.center(50))
+print()
+print()
+print(name.center(75))
+print()
+print()
+print(line4.center(100))
